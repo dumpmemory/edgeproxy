fd6e256161e4a28f125973c93cc5cacdddfba3b0
DJCrabhat <djcrabhat@sosimplerecords.com>
2022-03-17 07:59:16 -0700

    update tf

--- cloud/aws/main.tf
+++ cloud/aws/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "4.5.0"
     }
   }
@@ -9,6 +9,10 @@ terraform {
 
 provider "aws" {
   region = var.region
+}
+
+resource "aws_cloudwatch_log_group" "logs" {
+  name_prefix = "edgeproxy"
 }
 
 
@@ -25,10 +29,55 @@ module "ecs-fargate" {
   source  = "cn-terraform/ecs-fargate/aws"
   version = "2.0.34"
 
-  name_prefix         = "edgeproxy-example-service"
-  vpc_id              = module.base-network.vpc_id
-  container_image     = "ubuntu"
-  container_name      = "test"
+  name_prefix     = "edgeproxy-exmpl-"
+  vpc_id          = module.base-network.vpc_id
+  container_image = "${var.edgeproxy_image}:${var.edgeproxy_tag}"
+  container_name  = "edgeproxy"
+  command         = ["server", "--verbose"]
+
   public_subnets_ids  = module.base-network.public_subnets_ids
   private_subnets_ids = module.base-network.private_subnets_ids
+
+  default_certificate_arn = aws_acm_certificate.this.arn
+  port_mappings = [
+    {
+      "containerPort" : 9180,
+      hostPort : 9180,
+      "protocol" : "tcp"
+    }
+  ]
+
+  lb_target_group_health_check_path = "/readyz"
+  lb_https_ports = {
+    "default_http" : {
+      "listener_port" : 443,
+      "target_group_port" : 9180,
+      "target_group_protocol" : "HTTP"
+    }
+  }
+  lb_http_ports = {}
+
+  log_configuration = {
+    "logDriver" : "awslogs",
+    "options" : {
+      "awslogs-group" : "${aws_cloudwatch_log_group.logs.name}",
+      "awslogs-region" : var.region,
+      "awslogs-stream-prefix" : "streaming"
+    }
+  }
+}
+
+
+
+resource "aws_route53_record" "service" {
+
+  allow_overwrite = true
+  name            = var.service_domain_name
+  records         = [module.ecs-fargate.aws_lb_lb_dns_name]
+  ttl             = 60
+  type            = "CNAME"
+  zone_id         = data.aws_route53_zone.default.zone_id
+}
+output "aws_lb_lb_dns_name" {
+  value = module.ecs-fargate.aws_lb_lb_dns_name
 }
--- cloud/aws/variables.tf
+++ cloud/aws/variables.tf
@@ -3,3 +3,17 @@ variable "region" {}
 variable "availability_zones" {
   type = list(string)
 }
+
+variable "edgeproxy_image" {
+  default = "ghcr.io/djcrabhat/edgeproxy"
+}
+
+variable "edgeproxy_tag" {
+  default = "feature-aws-reference-arch"
+}
+
+variable "zone_name" {
+  description = "name of the r53 zone records will go in"
+}
+
+variable "service_domain_name" {}
